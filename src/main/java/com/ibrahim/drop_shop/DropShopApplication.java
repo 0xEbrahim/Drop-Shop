63adc5ecf704030524d7ad70167de29bf7c7393f
0xEbrahim <elsayedebrahim603@gmail.com>
2025-09-07 15:07:03 +0300

    init spring app

---
+++ src/main/java/com/ibrahim/drop_shop/DropShopApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahim.drop_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibrahim/drop_shop/DropShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.drop_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
