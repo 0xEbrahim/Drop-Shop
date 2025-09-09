709b4f9bca390f5d79ec6389686d9f956710c542
0xEbrahim <elsayedebrahim603@gmail.com>
2025-09-09 20:53:07 +0300

    feat: implement Cart controller

--- src/main/java/com/ibrahim/drop_shop/controllers/CartController.java
+++ src/main/java/com/ibrahim/drop_shop/controllers/CartController.java
@@ -1,23 +1,41 @@
 package com.ibrahim.drop_shop.controllers;
 
 
-import com.ibrahim.drop_shop.services.cart.CartService;
 import com.ibrahim.drop_shop.services.cart.DTO.CartResponseDto;
+import com.ibrahim.drop_shop.services.cart.ICartService;
 import com.ibrahim.drop_shop.utils.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("${api.prefix}/carts")
 public class CartController {
-    private final CartService cartService;
+    private final ICartService cartService;
 
-    public CartController(CartService cartService){
+    public CartController(ICartService cartService){
         this.cartService = cartService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse> getAllCarts() {
+        List<CartResponseDto> carts = cartService.getAllCarts();
+        return ApiResponse.sendResponse("Found", HttpStatus.OK, carts);
+    }
+
+    @GetMapping("price/{id}")
+    public ResponseEntity<ApiResponse> getCartTotalPrice(@PathVariable("id") Long id) {
+        BigDecimal totalPrice = cartService.getCartTotalPrice(id);
+        return ApiResponse.sendResponse("Success", HttpStatus.OK, totalPrice);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> clearCart(@PathVariable("id") Long id) {
+        cartService.clearCart(id);
+        return ApiResponse.sendResponse("Cart cleared successfully", HttpStatus.OK, null);
     }
 
     @GetMapping("/{id}")
