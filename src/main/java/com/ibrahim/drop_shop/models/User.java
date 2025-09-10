d40e0f3d82349ef2ab22f63fd12a517e4ea9a0e5
0xEbrahim <elsayedebrahim603@gmail.com>
2025-09-10 11:33:36 +0300

    core: add user entity

---
+++ src/main/java/com/ibrahim/drop_shop/models/User.java
@@ -0,0 +1,34 @@
+package com.ibrahim.drop_shop.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @OneToMany(mappedBy = "user",  cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders;
+}
