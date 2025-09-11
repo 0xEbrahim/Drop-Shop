612e2f24c508314570a546651de7cdedbe9597a6
0xEbrahim <elsayedebrahim603@gmail.com>
2025-09-11 22:30:12 +0300

    feat: implement custom user details service & custom user details

--- src/main/java/com/ibrahim/drop_shop/repositories/UserRepository.java
+++ src/main/java/com/ibrahim/drop_shop/repositories/UserRepository.java
@@ -3,6 +3,10 @@ package com.ibrahim.drop_shop.repositories;
 import com.ibrahim.drop_shop.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/ibrahim/drop_shop/security/CustomUserDetailsService.java
+++ src/main/java/com/ibrahim/drop_shop/security/CustomUserDetailsService.java
@@ -1,15 +1,26 @@
 package com.ibrahim.drop_shop.security;
 
 
-import org.springframework.security.core.userdetails.UserDetails;
+import com.ibrahim.drop_shop.exceptions.NotFoundException;
+import com.ibrahim.drop_shop.models.User;
+import com.ibrahim.drop_shop.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public CustomUserDetails loadUserByUsername(String email) throws NotFoundException {
+        User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new NotFoundException("User not found."));
+        return new CustomUserDetails(user);
     }
 }
